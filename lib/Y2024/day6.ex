799c6295837a471819f18178314600a4bfdb93e1
Brian Dixon <brianide@gmail.com>
2024-12-06 00:05:33 -0600

    Day 6 part 1

---
+++ lib/Y2024/day6.ex
@@ -0,0 +1,55 @@
+defmodule AOC.Y2024.Day6 do
+
+  def parse(input) do
+    String.graphemes(input)
+    |> Enum.reduce({MapSet.new(), 0, 0, 0, {0, 0}}, fn
+      "\n", {obs, r, c, _, st} -> {obs, r + 1, 0, c, st}
+      "^", {obs, r, c, wd, _} -> {obs, r, c + 1, wd, {r, c}}
+      "#", {obs, r, c, wd, st} -> {MapSet.put(obs, {r, c}), r, c + 1, wd, st}
+      ".", {obs, r, c, wd, st} -> {obs, r, c + 1, wd, st}
+    end)
+    |> then(fn {obs, r, _, wd, st} -> %{obs: obs, rows: r + 1, cols: wd, init: st} end)
+  end
+
+  def outside?(setup, r, c), do: r < 0 || r == setup.rows || c < 0 || c == setup.cols
+
+  def turn(:north), do: :east
+  def turn(:east), do: :south
+  def turn(:south), do: :west
+  def turn(:west), do: :north
+
+  def offset(:north), do: {-1, 0}
+  def offset(:east), do: {0, 1}
+  def offset(:south), do: {1, 0}
+  def offset(:west), do: {0, -1}
+
+  def walk(setup), do: walk(setup, setup.init, :north, MapSet.new())
+  def walk(setup, {r, c}, dir, seen) do
+    {dr, dc} = offset(dir)
+    {nr, nc} = {r + dr, c + dc}
+
+    seen = MapSet.put(seen, {r, c})
+
+    cond do
+      MapSet.member?(setup.obs, {nr, nc}) ->
+        walk(setup, {r, c}, turn(dir), seen)
+      outside?(setup, nr, nc) ->
+        seen
+      :else ->
+        walk(setup, {nr, nc}, dir, seen)
+    end
+  end
+
+  def silver(setup) do
+    walk(setup)
+    |> MapSet.size()
+    |> inspect()
+  end
+
+  def gold(_input) do
+    ""
+  end
+
+  def solver, do: AOC.Scaffold.chain_solver(&parse/1, &silver/1, &gold/1)
+
+end
--- lib/Y2024/index.ex
+++ lib/Y2024/index.ex
@@ -1,11 +1,10 @@
 defmodule AOC.Y2024.Index do
-  alias AOC.Y2024.Day1
-
   def solutions, do: [
     {Day1, "Historian Hysteria"},
     {Day2, "Red-Nosed Reports"},
     {Day3, "Mull It Over"},
     {Day4, "Ceres Search"},
     {Day5, "Print Queue"},
+    {AOC.Y2024.Day6, "Foo"}
   ]
 end
