956f84ebf5c7b41255420c3246efb9cec51f91a7
Brian Dixon <brianide@gmail.com>
2024-12-07 23:59:17 -0600

    Day 8 part 2, ezpz

--- lib/Y2024/day8.ex
+++ lib/Y2024/day8.ex
@@ -2,7 +2,7 @@ defmodule AOC.Y2024.Day8 do
   @moduledoc title: "Resonant Collinearity"
   @moduledoc url: "https://adventofcode.com/2024/day/8"
 
-  def solver, do: AOC.Scaffold.chain_solver(2024, 8, &parse/1, &silver/1, &gold/1)
+  def solver, do: AOC.Scaffold.chain_solver(2024, 8, &parse/1, &solve(&1, false), &solve(&1, true))
 
   def parse(input) do
     String.graphemes(input)
@@ -11,35 +11,39 @@ defmodule AOC.Y2024.Day8 do
       ".", {ant, r, c, wd} -> {ant, r, c + 1, wd}
       n, {ant, r, c, wd} -> {Map.update(ant, n, [{r, c}], &[{r, c} | &1]), r, c + 1, wd}
     end)
-    |> case do {ant, r, _, wd} -> %{cells: ant, rows: r + 1, cols: wd} end
+    |> case do {ant, r, _, wd} -> %{ants: ant, rows: r + 1, cols: wd} end
   end
 
-  def difference({ar, ac}, {br, bc}), do: {br - ar, bc - ac}
-  def add({r, c}, {dr, dc}), do: {r + dr, c + dc}
-  def negate({r, c}), do: {-r, -c}
+  defp add({r, c}, {dr, dc}), do: {r + dr, c + dc}
+  defp negate({r, c}), do: {-r, -c}
+  defp difference({ar, ac}, {br, bc}), do: {br - ar, bc - ac}
 
-  def find_antinodes(ants) do
-    for a <- ants,
-        b <- ants,
-        a !== b,
-        reduce: MapSet.new() do acc ->
-          diff = difference(a, b)
-
-          acc
-          |> MapSet.put(add(a, negate(diff)))
-          |> MapSet.put(add(b, diff))
-        end
+  defp plot_antinodes(seen, {r, c}, d, rows, cols, multi) do
+    if r < 0 || r >= rows || c < 0 || c >= cols do
+      seen
+    else
+      seen = MapSet.put(seen, {r, c})
+      if not multi, do: seen, else: plot_antinodes(seen, add({r, c}, d), d, rows, cols, multi)
+    end
   end
 
-  def silver(scen) do
-    Stream.flat_map(scen.cells, fn {_, ants} -> find_antinodes(ants) end)
-    |> MapSet.new()
-    |> Enum.count(fn {r, c} -> r >= 0 && r < scen.rows && c >= 0 && c < scen.cols end)
-    |> inspect(charlists: :as_lists)
-  end
+  defp solve(scen, multi) do
+    Enum.reduce(scen.ants, MapSet.new(), fn {_, ants}, seen ->
+      for a <- ants,
+          b <- ants,
+          a !== b,
+          reduce: seen do acc ->
+            diff = difference(a, b)
+            rev = negate(diff)
+            a = if multi, do: a, else: add(a, rev)
+            b = if multi, do: b, else: add(b, diff)
 
-  def gold(_scen) do
-    "Not implemented"
+            acc
+            |> plot_antinodes(a, rev, scen.rows, scen.cols, multi)
+            |> plot_antinodes(b, diff, scen.rows, scen.cols, multi)
+          end
+    end)
+    |> MapSet.size()
   end
 
 end
