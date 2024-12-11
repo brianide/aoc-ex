9402ecff01a58be584fdb369d5753baa09562f40
Brian Dixon <brianide@gmail.com>
2024-12-10 23:55:48 -0600

    Day 11 part 2 (memoized)

--- lib/Y2024/day11.ex
+++ lib/Y2024/day11.ex
@@ -2,36 +2,30 @@ defmodule AOC.Y2024.Day11 do
   @moduledoc title: "Plutonian Pebbles"
   @moduledoc url: "https://adventofcode.com/2024/day/11"
 
-  def solver, do: AOC.Scaffold.chain_solver(2024, 11, &parse/1, &silver/1, &gold/1)
+  use Memoize
+
+  def solver, do: AOC.Scaffold.chain_solver(2024, 11, &parse/1, &solve(&1, 25), &solve(&1, 75))
 
   def parse(input), do: Regex.scan(~r/\d+/, input) |> Enum.map(fn ss -> List.first(ss) |> String.to_integer() end)
 
   defp digits(a), do: :math.log10(a + 1) |> ceil()
 
-  defp update_stone(0), do: [1]
-  defp update_stone(n) do
+  defp update_stone(_, 0), do: 1
+  defp update_stone(0, r), do: update_stone(1, r - 1)
+  defmemop update_stone(n, r) do
     len = digits(n)
     if rem(len, 2) === 0 do
       d = 10 ** div(len, 2)
-      [div(n, d), rem(n, d)]
+      update_stone(div(n, d), r - 1) + update_stone(rem(n, d), r - 1)
     else
-      [n * 2024]
+      update_stone(n * 2024, r - 1)
     end
   end
 
-  defp update(list, 0), do: list
-  defp update(list, n) do
-    list
-    |> Enum.flat_map(&update_stone/1)
-    |> update(n - 1)
-  end
-
-  def silver(input) do
-    update(input, 25) |> length()
-  end
-
-  def gold(_input) do
-    "Not implemented"
+  def solve(input, depth) do
+    for st <- input,
+        reduce: 0,
+        do: (acc -> acc + update_stone(st, depth))
   end
 
 end
