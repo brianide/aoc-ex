ae8ce19c2e2c49b2c39916096117bce78064c4ed
Brian Dixon <brianide@gmail.com>
2024-12-13 01:23:54 -0600

    Day 13 part 2
    Advent of Wolfram Alpha

--- lib/Y2024/day13.ex
+++ lib/Y2024/day13.ex
@@ -2,7 +2,7 @@ defmodule AOC.Y2024.Day13 do
   @moduledoc title: "Claw Contraption"
   @moduledoc url: "https://adventofcode.com/2024/day/13"
 
-  def solver, do: AOC.Scaffold.chain_solver(2024, 13, &parse/1, &silver/1, &gold/1)
+  def solver, do: AOC.Scaffold.chain_solver(2024, 13, &parse/1, &solve/1, &solve(&1, 10 ** 13))
 
   def parse(input) do
     Regex.scan(~r/Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)/, input)
@@ -10,28 +10,19 @@ defmodule AOC.Y2024.Day13 do
     |> Enum.map(fn [ax, ay, bx, by, tx, ty] -> {{ax, ay}, {bx, by}, {tx, ty}} end)
   end
 
-  def solve_case({{ax, ay}, {bx, by}, {tx, ty}}) do
-    (for ac <- 0..100,
-        bc <- 0..100,
-        ax * ac + bx * bc === tx,
-        ay * ac + by * bc === ty,
-        do: ac * 3 + bc)
-    |> Enum.sort(:asc)
-    |> List.first()
-  end
-
-  def silver(input) do
+  def solve(input, add \\ 0) do
     input
-    |> Task.async_stream(&solve_case/1)
-    |> Enum.reduce(0, fn
-      {:ok, n}, acc when not is_nil(n) -> acc + n
-      _, acc -> acc
-    end)
-    |> inspect(charlists: :as_lists)
-  end
+    |> Stream.filter(fn {{ax, ay}, {bx, by}, _} -> bx * ay !== ax * by && bx * by !== 0 end)
+    |> Stream.map(fn {{ax, ay}, {bx, by}, {tx, ty}} ->
+      tx = tx + add
+      ty = ty + add
+      ac = div((tx * by - bx * ty), (ax * by - bx * ay))
+      bc = div((ty - ac * ay), by)
+      eq = tx === ac * ax + bc * bx && ty === ac * ay + bc * by
 
-  def gold(_input) do
-    "Not implemented"
+      if eq, do: ac * 3 + bc, else: 0
+    end)
+    |> Enum.sum()
   end
 
 end
