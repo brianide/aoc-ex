4f068e046e5ad1ddc8858c6b5e2411bf7e1459de
= <brianide@gmail.com>
2024-12-18 22:47:45 -0600

    Day 16 DFS attempt (bad)

--- lib/Y2024/day16.ex
+++ lib/Y2024/day16.ex
@@ -2,6 +2,8 @@ defmodule AOC.Y2024.Day16 do
   @moduledoc title: "Reindeer Maze"
   @moduledoc url: "https://adventofcode.com/2024/day/16"
 
+  alias PriorityQueue, as: PQ
+
   def solver, do: AOC.Scaffold.chain_solver(2024, 16, &parse/1, &silver/1, &gold/1)
 
   def parse(input) do
@@ -10,39 +12,66 @@ defmodule AOC.Y2024.Day16 do
     end
   end
 
-  defp neighbors(walls, {r, c}, visited) do
+  defp neighbors({r, c}) do
     for {dir, dr, dc} <- [{:north, -1, 0}, {:south, 1, 0}, {:east, 0, 1}, {:west, 0, -1}],
         r = r + dr,
         c = c + dc,
-        {r, c} not in walls,
-        {r, c} not in visited,
         do: {{r, c}, dir}
   end
 
+  defp turn_cost(dir, n_dir) do
+    case {dir, n_dir} do
+      {d, n} when d === n -> 0
+      tp when tp in [{:north, :east}, {:east, :south}, {:south, :west}, {:west, :north}] -> 1000
+      tp when tp in [{:north, :west}, {:west, :south}, {:south, :east}, {:east, :north}] -> 1000
+      tp when tp in [{:north, :south}, {:south, :north}, {:east, :west}, {:west, :east}] -> 2000
+    end
+  end
+
   defp search(queue, finish, walls, visited \\ MapSet.new()) do
-    {{score, {pos, dir}}, queue} = PriorityQueue.pop!(queue)
+    {{score, {pos, dir}}, queue} = PQ.pop(queue)
     if pos === finish do
       score
     else
       visited = MapSet.put(visited, pos)
-      for {n_pos, n_dir} <- neighbors(walls, pos, visited),
+      for {n_pos, n_dir} <- neighbors(pos),
+          n_pos not in walls,
+          n_pos not in visited,
           reduce: queue do queue ->
-            n_score = if n_dir === dir, do: score + 1, else: score + 1001
-            PriorityQueue.put(queue, n_score, {n_pos, n_dir})
+            score = score + 1 + turn_cost(dir, n_dir)
+            PQ.put(queue, score, {n_pos, n_dir})
           end
       |> search(finish, walls, visited)
     end
   end
 
   def silver({walls, start, finish}) do
-    PriorityQueue.new()
-    |> PriorityQueue.put(0, {start, :east})
+    PQ.new()
+    |> PQ.put(0, {start, :east})
     |> search(finish, walls)
     |> inspect(charlists: :as_lists)
   end
 
-  def gold(_input) do
-    "Not implemented"
+  def dfs({pos, _}, _, finish, _, seen) when pos === finish, do: seen
+  def dfs(_, _, _, n, _) when n < 0, do: nil
+  def dfs({pos, dir}, walls, finish, ttl, seen) do
+    seen = MapSet.put(seen, pos)
+    for {n_pos, n_dir} <- neighbors(pos),
+        n_pos not in walls,
+        n_pos not in seen,
+        reduce: MapSet.new() do acc ->
+          case dfs({n_pos, n_dir}, walls, finish, ttl - turn_cost(dir, n_dir) - 1, seen) do
+            nil -> acc
+            n -> MapSet.union(acc, n)
+          end
+        end
+  end
+
+  def gold({walls, start, finish}) do
+    dfs({start, :east}, walls, finish, 98520, MapSet.new())
+    |> MapSet.size()
+    |> then(&(&1 + 1))
+    |> inspect()
   end
 
 end
