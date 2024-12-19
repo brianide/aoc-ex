8cff4e8147902a583d9b360a4e5f4351539f3d3c
Brian Dixon <brianide@gmail.com>
2024-12-19 00:26:40 -0600

    Day 19 but now it's fast

--- lib/Y2024/day19.ex
+++ lib/Y2024/day19.ex
@@ -2,8 +2,6 @@ defmodule AOC.Y2024.Day19 do
   @moduledoc title: "Linen Layout"
   @moduledoc url: "https://adventofcode.com/2024/day/19"
 
-  use Memoize
-
   def solver, do: AOC.Scaffold.double_solver(2024, 19, &parse/1, &solve/1)
 
   def parse(input) do
@@ -15,18 +13,25 @@ defmodule AOC.Y2024.Day19 do
   end
 
   defp verify([], _), do: 1
-  defmemop verify(rest, towels) do
-    for towel <- towels,
-        List.starts_with?(rest, towel),
-        reduce: 0 do acc ->
-          rest = Enum.drop(rest, length(towel))
-          acc + verify(rest, towels)
-        end
+  defp verify(rest, towels) do
+    case Process.get(rest) do
+      nil ->
+        for towel <- towels,
+            List.starts_with?(rest, towel),
+            reduce: 0 do acc ->
+              rest = Enum.drop(rest, length(towel))
+              acc + verify(rest, towels)
+            end
+        |> tap(&Process.put(rest, &1))
+      prev -> prev
+    end
   end
 
   def solve({towels, designs}) do
-    counts = Enum.map(designs, &verify(&1, towels))
-    {Enum.count(counts, &(&1 > 0)), Enum.sum(counts)}
+    for {:ok, count} <- Task.async_stream(designs, &verify(&1, towels)),
+        reduce: {0, 0} do {valid, total} ->
+          {(if count > 0, do: valid + 1, else: valid), total + count}
+        end
   end
 
 end
