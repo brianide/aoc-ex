f3aeba7e191110a22c65ffbe2a1ea1ac48350637
Brian Dixon <brianide@gmail.com>
2024-12-20 07:02:08 -0600

    Refactoring

--- lib/Y2024/day20.ex
+++ lib/Y2024/day20.ex
@@ -5,9 +5,8 @@ defmodule AOC.Y2024.Day20 do
   def solver, do: AOC.Scaffold.double_solver(2024, 20, &parse/1, &solve/1)
 
   def parse(input) do
-    case AOC.Util.parse_grid(input) do
-      {%{"#" => w, "E" => [e]}, r, c} ->
-        {%{walls: w, rows: r, cols: c}, e}
+    case AOC.Util.parse_grid(input, dims: false) do
+      %{"#" => w, "S" => [s], "E" => [e]} -> {w, s, e}
     end
   end
 
@@ -19,16 +18,16 @@ defmodule AOC.Y2024.Day20 do
         end
   end
 
-  defp bfs(start, map), do: bfs([start], map, 1, %{start => 0})
-  defp bfs([], _, _, dists), do: dists
-  defp bfs(queue, map, depth, dists) do
+  defp bfs(walls, start, goal), do: bfs([start], walls, goal, 1, %{start => 0})
+  defp bfs(_, _, goal, _, dists) when is_map_key(dists, goal), do: dists
+  defp bfs(queue, walls, goal, depth, dists) do
     for curr <- queue,
         pos <- neighbors(curr),
-        pos not in map.walls,
+        pos not in walls,
         not is_map_key(dists, pos),
         into: %{} do {pos, depth} end
     |> case do
-      ns -> bfs(Map.keys(ns), map, depth + 1, Map.merge(dists, ns))
+      ns -> bfs(Map.keys(ns), walls, goal, depth + 1, Map.merge(dists, ns))
     end
   end
 
@@ -52,6 +51,6 @@ defmodule AOC.Y2024.Day20 do
     end)
   end
 
-  def solve({map, finish}), do: bfs(finish, map) |> check_skips()
+  def solve({walls, start, finish}), do: bfs(walls, finish, start) |> check_skips()
 
 end
