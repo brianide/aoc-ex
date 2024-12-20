2189572604980d25ecbe134e8060bebfe74f3368
= <brianide@gmail.com>
2024-12-20 05:53:52 -0600

    Day 20 part 2
    Cool problem

--- lib/Y2024/day20.ex
+++ lib/Y2024/day20.ex
@@ -2,12 +2,12 @@ defmodule AOC.Y2024.Day20 do
   @moduledoc title: "Race Condition"
   @moduledoc url: "https://adventofcode.com/2024/day/20"
 
-  def solver, do: AOC.Scaffold.chain_solver(2024, 20, &parse/1, &silver/1, &gold/1)
+  def solver, do: AOC.Scaffold.double_solver(2024, 20, &parse/1, &solve/1)
 
   def parse(input) do
     case AOC.Util.parse_grid(input) do
-      {%{"#" => w, "S" => [s], "E" => [e]}, r, c} ->
-        {%{walls: w, rows: r, cols: c}, {s, e}}
+      {%{"#" => w, "E" => [e]}, r, c} ->
+        {%{walls: w, rows: r, cols: c}, e}
     end
   end
 
@@ -32,54 +32,24 @@ defmodule AOC.Y2024.Day20 do
     end
   end
 
-  def check_skips(%{walls: walls, rows: rm, cols: cm}, dists) do
-    for {wr, wc} <- walls,
-        wr > 0 && wr < rm - 1,
-        wc > 0 && wc < cm - 1,
-        sides = neighbors({wr, wc}),
-        na <- sides,
-        nb <- sides,
-        na !== nb,
-        na not in walls,
-        nb not in walls,
-        da = Map.get(dists, na),
-        db = Map.get(dists, nb),
-        saved = db - da - 2,
-        # saved > 0,
-        # do: {{na, nb}, saved}
-        saved >= 100,
-        reduce: 0, do: (acc -> acc + 1)
+  defp manhattan_dist({ra, ca}, {rb, cb}), do: abs(rb - ra) + abs(cb - ca)
+
+  @threshold 100
+  defp check_skips(dists) do
+    for {pa, da} <- dists,
+        {pb, db} <- dists,
+        pa !== pb,
+        dist = manhattan_dist(pa, pb),
+        db - da - dist >= @threshold,
+        reduce: {0, 0} do {s, g} ->
+          case dist do
+            2 -> {s + 1, g + 1}
+            n when n <= 20 -> {s, g + 1}
+            _ -> {s, g}
+          end
+        end
   end
 
-  # def show_skips(skips, %{walls: walls, rows: rm, cols: cm}, start, finish) do
-  #   Stream.map(0..(rm - 1), fn r ->
-  #     Stream.map(0..(cm - 1), fn c ->
-  #       cond do
-  #         {r, c} === start -> "S"
-  #         {r, c} === finish -> "E"
-  #         {r, c} in walls -> '#'
-  #         {r, c} -> '.'
-  #       end
-  #     end)
-  #     |> Enum.join("")
-  #   end)
-  #   |> Enum.join("\n")
-  #   |> IO.puts()
-  # end
-
-  def silver({map, {start, finish}}) do
-    dists = bfs(finish, map)
-    check_skips(map, dists)
-
-    # show_skips(nil, map, start, finish)
-    # |> Enum.group_by(&elem(&1, 1), fn _ -> 1 end)
-    # |> Enum.map(fn {k, v} -> {k, length(v)} end)
-    # |> Enum.sort(:desc)
-    # |> inspect(charlists: :as_lists)
-  end
-
-  def gold(_input) do
-    "Not implemented"
-  end
+  def solve({map, finish}), do: bfs(finish, map) |> check_skips()
 
 end
