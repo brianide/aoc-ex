aaa2cd53a203aeef7b9f81ec3f7efe76cc21c020
= <brianide@gmail.com>
2024-12-20 19:18:27 -0600

    Forgot to make a MapSet

--- lib/Y2024/day20.ex
+++ lib/Y2024/day20.ex
@@ -5,8 +5,9 @@ defmodule AOC.Y2024.Day20 do
   def solver, do: AOC.Scaffold.double_solver(2024, 20, &parse/1, &solve/1)
 
   def parse(input) do
-    case AOC.Util.parse_grid(input, dims: false) do
-      %{"#" => w, "S" => [s], "E" => [e]} -> {w, s, e}
+    case AOC.Util.parse_grid(input, dims: false, ignore: ["#"]) do
+      %{"." => f, "S" => [s], "E" => [e]} ->
+        {MapSet.new([s, e | f]), s, e}
     end
   end
 
@@ -18,16 +19,16 @@ defmodule AOC.Y2024.Day20 do
         end
   end
 
-  defp bfs(walls, start, goal), do: bfs([start], walls, goal, 1, %{start => 0})
+  defp bfs(tiles, start, goal), do: bfs([start], tiles, goal, 1, %{start => 0})
   defp bfs(_, _, goal, _, dists) when is_map_key(dists, goal), do: dists
-  defp bfs(queue, walls, goal, depth, dists) do
+  defp bfs(queue, tiles, goal, depth, dists) do
     for curr <- queue,
         pos <- neighbors(curr),
-        pos not in walls,
+        pos in tiles,
         not is_map_key(dists, pos),
         into: %{} do {pos, depth} end
     |> case do
-      ns -> bfs(Map.keys(ns), walls, goal, depth + 1, Map.merge(dists, ns))
+      ns -> bfs(Map.keys(ns), tiles, goal, depth + 1, Map.merge(dists, ns))
     end
   end
 
@@ -51,6 +52,6 @@ defmodule AOC.Y2024.Day20 do
     end)
   end
 
-  def solve({walls, start, finish}), do: bfs(walls, finish, start) |> check_skips()
+  def solve({tiles, start, finish}), do: bfs(tiles, finish, start) |> check_skips()
 
 end
