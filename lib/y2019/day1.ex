89dfadac8469d4cbe081fdb427c29d40ff875e67
= <brianide@gmail.com>
2025-01-03 08:35:26 -0600

    2019 day 1

--- lib/y2019/day1.ex
+++ lib/y2019/day1.ex
@@ -9,18 +9,12 @@ defmodule AOC.Y2019.Day1 do
   end
 
   def silver(input) do
-    for n <- input, reduce: 0 do acc -> acc + div(n, 3) - 2 end
+    # for n <- input, reduce: 0 do acc -> acc + div(n, 2) - 2 end
+    inspect(input)
   end
 
-  def calc_fuel(mass) do
-    case div(mass, 3) - 2 do
-      n when n <= 0 -> 0
-      n -> n + calc_fuel(n)
-    end
-  end
-
-  def gold(input) do
-    for n <- input, reduce: 0 do acc -> acc + calc_fuel(n) end
+  def gold(_input) do
+    "Not implemented"
   end
 
 end
