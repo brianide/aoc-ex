f9d8578115c39d8eb1211945df5b30d1130f96a1
= <brianide@gmail.com>
2025-08-25 20:36:04 -0500

    Day 7 part 2
    Pure functional memoization is confusing

--- lib/aoc/y2015/day7.ex
+++ lib/aoc/y2015/day7.ex
@@ -2,7 +2,7 @@ defmodule AOC.Y2015.Day7 do
   @moduledoc title: "Some Assembly Required"
   @moduledoc url: "https://adventofcode.com/2015/day/7"
 
-  use AOC.Solvers.Chain, [2015, 7, &parse/1, &silver/1, &gold/1]
+  use AOC.Solvers.Double, [2015, 7, &parse/1, &solve/1]
 
   alias Bitwise, as: Bit
 
@@ -18,39 +18,48 @@ defmodule AOC.Y2015.Day7 do
         tokens = Regex.scan(~r/\w+/, line, capture: :first),
         tokens = Enum.map(tokens, &List.first/1) do
           case tokens do
-            [a, r] -> {r, {:assign, parse_term(a)}}
-            [a, "AND", b, r] -> {r, {:and, parse_term(a), parse_term(b)}}
-            [a, "OR", b, r] -> {r, {:or, parse_term(a), parse_term(b)}}
-            [a, "LSHIFT", b, r] -> {r, {:lshift, parse_term(a), parse_term(b)}}
-            [a, "RSHIFT", b, r] -> {r, {:rshift, parse_term(a), parse_term(b)}}
-            ["NOT", a, r] -> {r, {:not, parse_term(a)}}
+            [a, r] -> {r, {:una, &Function.identity/1, parse_term(a)}}
+            [a, "AND", b, r] -> {r, {:bin, &Bit.band/2, parse_term(a), parse_term(b)}}
+            [a, "OR", b, r] -> {r, {:bin, &Bit.bor/2, parse_term(a), parse_term(b)}}
+            [a, "LSHIFT", b, r] -> {r, {:bin, &Bit.bsl/2, parse_term(a), parse_term(b)}}
+            [a, "RSHIFT", b, r] -> {r, {:bin, &Bit.bsr/2, parse_term(a), parse_term(b)}}
+            ["NOT", a, r] -> {r, {:una, &Bit.bnot/1, parse_term(a)}}
           end
         end
         |> Map.new()
   end
 
-  def simplify({:symbol, term}, table) do
-    case Process.get(term) do
-      nil ->
-        res = simplify(table[term], table)
-        Process.put(term, res)
-        res
-      v -> v
+  def simplify(table) do
+    simplify({:symbol, "a"}, table, %{})
+    |> then(&elem(&1, 0))
+  end
+
+  def simplify({:const, n}, _, mem), do: {n, mem}
+
+  def simplify({:symbol, term}, table, mem) do
+    if is_map_key(mem, term) do
+      {mem[term], mem}
+    else
+      {res, mem} = simplify(table[term], table, mem)
+      {res, put_in(mem[term], res)}
     end
   end
 
-  def simplify({:const, n}, _), do: n
-  def simplify({:assign, a}, table), do: simplify(a, table)
-  def simplify({:and, a, b}, table), do: Bit.band(simplify(a, table), simplify(b, table))
-  def simplify({:or, a, b}, table), do: Bit.bor(simplify(a, table), simplify(b, table))
-  def simplify({:lshift, a, b}, table), do: Bit.bsl(simplify(a, table), simplify(b, table))
-  def simplify({:rshift, a, b}, table), do: Bit.bsr(simplify(a, table), simplify(b, table))
-  def simplify({:not, a}, table), do: Bit.bnot(simplify(a, table))
+  def simplify({:una, op, a}, table, mem) do
+    {n, mem} = simplify(a, table, mem)
+    {op.(n), mem}
+  end
 
-  def silver(table), do: simplify({:symbol, "a"}, table)
+  def simplify({:bin, op, a, b}, table, mem) do
+    {a, mem} = simplify(a, table, mem)
+    {b, mem} = simplify(b, table, mem)
+    {op.(a, b), mem}
+  end
 
-  def gold(_input) do
-    "Not implemented"
+  def solve(table) do
+    silv = simplify(table)
+    gold = simplify(put_in(table["b"], {:const, silv}))
+    {silv, gold}
   end
 
 end
