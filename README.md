e4a0de4998dd7981b48efbc0f5c58171e00f815e
= <brianide@gmail.com>
2025-08-26 02:11:48 -0500

    Better benchmarking

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # aoc-ex
 Advent of Code solutions in Elixir
 
-Run solutions with `mix aoc.solve -r <input_root> -p <part> -y <year> -d <day> [--bench]`.
+Run solutions with `mix aoc.solve -r <input_root> -p <part> -y <year> -d <day> [--bench <cycles>]`.
 
 The `part` argument should be `silver`, `gold`, or `both`, or just the first letter of the respective word.
 
--- lib/mix/tasks/aoc.solve.ex
+++ lib/mix/tasks/aoc.solve.ex
@@ -5,16 +5,28 @@ defmodule Mix.Tasks.Aoc.Solve do
     {opts, _} =
       OptionParser.parse!(
         args,
-        strict: [year: :integer, day: :integer, root: :string, part: :string, bench: :boolean],
+        strict: [year: :integer, day: :integer, root: :string, part: :string, bench: :integer],
         aliases: [y: :year, d: :day, r: :root, p: :part, b: :bench]
       )
 
     String.to_existing_atom("Elixir.AOC.Y#{opts[:year]}.Day#{opts[:day]}")
     |> Code.ensure_loaded!()
     |> case do mod ->
-      {time, res} = apply(mod, :solve_day, [opts[:root], opts[:part]])
-      IO.puts(res)
-      if opts[:bench], do: IO.puts("\nRan in #{time / 1000}ms")
+      case opts[:bench] do
+        nil ->
+          {_, res} = apply(mod, :solve_day, [opts[:root], opts[:part]])
+          IO.puts(res)
+
+        count ->
+          for _ <- 1..count, reduce: {0, nil} do {time, _res} ->
+            {t, r} = apply(mod, :solve_day, [opts[:root], opts[:part]])
+            {time + t, r}
+          end
+          |> case do {time, res} ->
+            label = if count > 1, do: "Average runtime:", else: "Ran in"
+            IO.puts("#{res}\n\n#{label} #{time / count / 1000}ms")
+          end
+      end
     end
   end
 end
