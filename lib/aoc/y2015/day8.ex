5d766fb889f55f127cf4496c1d10422c8b114ad3
= <brianide@gmail.com>
2025-08-25 22:27:15 -0500

    Refactoring

--- lib/aoc/y2015/day8.ex
+++ lib/aoc/y2015/day8.ex
@@ -2,31 +2,19 @@ defmodule AOC.Y2015.Day8 do
   @moduledoc title: "Matchsticks"
   @moduledoc url: "https://adventofcode.com/2015/day/8"
 
-  use AOC.Solvers.Chain, [2015, 8, &parse/1, &silver/1, &gold/1]
+  use AOC.Solvers.Double, [2015, 8, &parse/1, &solve/1]
 
   def parse(input), do: String.splitter(input, "\n", trim: true)
 
-  def silver(input) do
-    Enum.reduce(input, 0, fn
-      str, acc ->
+  def solve(input) do
+    Enum.reduce(input, {0, 0}, fn
+      str, {s, g} ->
         a = String.length(str)
         b = (Regex.scan(~r/\\\\|\\"|\\x..|./, str) |> length()) - 2
-        acc + a - b
-    end)
-  end
 
-  def gold(input) do
-    Enum.reduce(input, 0, fn str, acc ->
-      a = String.length(str)
-      b =
-        for match <- Regex.scan(~r/(\\|")|(.)/, str, capture: :all_but_first),
-            reduce: 2 do acc ->
-              case length(match) do
-                1 -> acc + 2
-                2 -> acc + 1
-              end
-            end
-      acc + b - a
+        c = String.to_charlist(str) |> Enum.count(&(&1 in [?\\, ?"]))
+
+        {s + a - b, g + c + 2}
     end)
   end
 
