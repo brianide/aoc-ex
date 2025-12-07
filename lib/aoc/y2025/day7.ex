77b982a164a52da8b58d1270f96d3bb8dee51636
= <brianide@gmail.com>
2025-12-07 00:56:26 -0600

    More day 7 refactoring

--- lib/aoc/y2025/day7.ex
+++ lib/aoc/y2025/day7.ex
@@ -3,19 +3,19 @@ defmodule AOC.Y2025.Day7 do
     title: "Laboratories",
     url: "https://adventofcode.com/2025/day/7",
     scheme: {:once, &parse/1, &solve/1},
-    complete: false
+    complete: true
 
   def parse(input) do
     for line <- String.split(input) |> Stream.drop(1) |> Stream.drop_every(2),
         line = String.to_charlist(line),
         reduce: [] do
       acc ->
-        Enum.zip_reduce(line, Stream.iterate(0, &(&1 + 1)), [], fn
-          ?^, ind, acc -> [ind | acc]
+        Enum.zip_reduce(line, Stream.iterate(0, &(&1 + 1)), MapSet.new(), fn
+          ?^, ind, acc -> MapSet.put(acc, ind)
           _, _, acc -> acc
         end)
         |> case do
-          inds -> [MapSet.new(inds) | acc]
+          inds -> [inds | acc]
         end
     end
     |> Enum.reverse()
@@ -27,22 +27,22 @@ defmodule AOC.Y2025.Day7 do
   def tally(beams, [], total), do: {total, Enum.sum_by(beams, &elem(&1, 1))}
 
   def tally(beams, [splits | lines], total) do
-    for {ind, n} = beam <- beams, reduce: {[], 0} do
+    for {ind, n} <- beams, reduce: {%{}, 0} do
       {acc, count} ->
         if MapSet.member?(splits, ind) do
-          left = {ind - 1, n}
-          right = {ind + 1, n}
-          {[left, right | acc], count + 1}
+          acc =
+            acc
+            |> Map.update(ind - 1, n, &(&1 + n))
+            |> Map.update(ind + 1, n, &(&1 + n))
+
+          {acc, count + 1}
         else
-          {[beam | acc], count}
+          acc = Map.update(acc, ind, n, &(&1 + n))
+          {acc, count}
         end
     end
     |> case do
-      {beams, count} ->
-        for {ind, n} <- beams, reduce: %{} do
-          acc -> Map.update(acc, ind, n, &(&1 + n))
-        end
-        |> tally(lines, total + count)
+      {beams, count} -> tally(beams, lines, total + count)
     end
   end
 
