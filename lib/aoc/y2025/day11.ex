64c558d81fd78f5fa37c9a2e9c31892847d430d2
Brian Dixon <brianide@gmail.com>
2025-12-11 00:56:53 -0600

    Day 11 part 2

--- lib/aoc/y2025/day11.ex
+++ lib/aoc/y2025/day11.ex
@@ -2,21 +2,22 @@ defmodule AOC.Y2025.Day11 do
   use AOC.Solution,
     title: "Reactor",
     url: "https://adventofcode.com/2025/day/11",
-    scheme: {:shared, &parse/1, &silver/1, &gold/1},
+    scheme: {:shared, &parse/1, &solve(&1, "you"), &solve(&1, "svr", true)},
     newline: :preserve,
-    complete: false
+    complete: true
 
   def skip_char(<<_, rest::binary>>), do: rest
 
   def read_label(<<label::binary-size(3), rest::binary>>), do: {label, rest}
 
-  @spec read_outputs(any(), any(), nonempty_binary()) :: {any(), binary()}
   def read_outputs(acc, dev, bin) do
     case bin do
       <<?\n, rest::binary>> ->
         {acc, rest}
+
       <<?\s, rest::binary>> ->
         {out, rest} = read_label(rest)
+
         Map.update(acc, dev, [out], &[out | &1])
         |> read_outputs(dev, rest)
     end
@@ -26,6 +27,7 @@ defmodule AOC.Y2025.Day11 do
     case bin do
       <<>> ->
         acc
+
       bin ->
         {dev, rest} = read_label(bin)
         {acc, rest} = read_outputs(acc, dev, skip_char(rest))
@@ -33,26 +35,41 @@ defmodule AOC.Y2025.Day11 do
     end
   end
 
-  def dfs(curr, map, seen \\ MapSet.new()) do
+  def dfs(curr, map, gold, mem \\ %{}, seen \\ MapSet.new()) do
+    {key, pass} =
+      if gold do
+        has_dac = MapSet.member?(seen, "dac")
+        has_fft = MapSet.member?(seen, "fft")
+        {{curr, has_dac, has_fft}, has_dac and has_fft}
+      else
+        {curr, true}
+      end
+
     cond do
-      curr === "out" ->
-        1
       MapSet.member?(seen, curr) ->
-        0
+        {0, mem}
+
+      is_map_key(mem, key) ->
+        {mem[key], mem}
+
+      curr === "out" and pass ->
+        {1, mem}
+
       :else ->
-        MapSet.put(seen, curr)
-        for neigh <- Map.get(map, curr, []), reduce: 0 do
-          acc -> acc + dfs(neigh, map, seen)
+        seen = MapSet.put(seen, curr)
+
+        for neigh <- Map.get(map, curr, []), reduce: {0, mem} do
+          {total, mem} ->
+            {res, mem} = dfs(neigh, map, gold, mem, seen)
+            {total + res, mem}
+        end
+        |> case do
+          {res, mem} -> {res, put_in(mem[key], res)}
         end
     end
   end
 
-  def silver(input) do
-    dfs("you", input)
+  def solve(input, start, gold \\ false) do
+    dfs(start, input, gold) |> elem(0)
   end
-
-  def gold(_input) do
-    "Not implemented"
-  end
-
 end
